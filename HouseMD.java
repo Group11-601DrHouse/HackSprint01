5b7f4dc55b903113f5282bc5818cec0bbb872064
Никита Ясавиев < >
2016-10-27 14:32:32 +0300

    ver 2.0

--- HouseMD.java
+++ HouseMD.java
@@ -3,39 +3,47 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class HouseMD {
+	
 	public static void main(String [] args) throws FileNotFoundException {
+		
 		Scanner console = new Scanner(System.in);
 		Scanner sympt = new Scanner (new File("Symptoms.txt"));
 		Scanner ill = new Scanner (new File("Illnesses.txt"));
 		Scanner prescript = new Scanner (new File("Prescriptions.txt"));
-		Scanner analys = new Scanner (new File("Analysis.txt"));
-		String analysis = "";
+		String choice = "";
 		String prescription = "";
 		String illness = "";
 		String symptom = "";
+		String symptomShown = "";
 		String result = "";
-		System.out.println("Tell me your symptoms & analysis(In the following sequence: 'Symptom, analysis')");
-		String input = console.nextLine();
-		input = input.toLowerCase();
-		String [] splArr = input.split(",");
-			do {
-				illness = ill.nextLine().toLowerCase();
-				prescription = prescript.nextLine().toLowerCase();
-				symptom = sympt.nextLine().toLowerCase();
-				analysis = analys.nextLine().toLowerCase();
-				analysis = " " + analysis;
-				if ((symptom.equals(splArr[0]))&&(analysis.equals(splArr[1]))) {
-					result ="You have " +  illness + ". Prescriptions: " + prescription;
+		System.out.println("Tell me your symptoms");
+		String input = console.nextLine().toLowerCase();
+		do {
+			illness = ill.nextLine();
+			prescription = prescript.nextLine();
+			symptomShown = sympt.nextLine();
+			symptom = symptomShown.toLowerCase();
+			if (symptom.startsWith(input)) {
+				System.out.println("Are these your symptoms?" + "\n" + symptomShown);
+				System.out.println("Y / N");
+				choice = console.nextLine().toLowerCase();
+				if (choice.equals("y")){
+					result ="You have " +  illness + "." + "\n" + "Prescriptions: " + "\n" + prescription;
+				}
+				else if (choice.equals("n")) {
+					continue;
 				}
 			}
-			while  ((sympt.hasNextLine())&&(analys.hasNextLine()));
-		
-		
-		if (result.equals("")){ 
-			System.out.println("Input is incorrect"); 
-		} 
-		else { 
-			System.out.println(result); 
 		}
-	}	
+		while  (sympt.hasNextLine());
+		if (result.equals("")){
+			System.out.println("I'm sorry, we don't have such symptoms in our database.");
+			System.out.println("Please, come back later, we'll certainly add them to our knowledge base:)");
+		}
+		else {
+			System.out.println(result);
+		}
+		
+	}
+	
 }
